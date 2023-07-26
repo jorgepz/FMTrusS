cbb70d833f7f1b763c26015624fa2b99f8d5c982
jorgepz <jorgepz@fing.edu.uy>
2023-07-26 20:29:45 -0300

    add example 6

---
+++ examples/Example_6.m
@@ -0,0 +1,72 @@
+% Copyright 2022, Jorge M. Perez Zerpa. 
+%
+% This file is part of FMTS.
+%
+% FMTS is free software: you can redistribute it and/or modify 
+% it under the terms of the GNU General Public License as published by 
+% the Free Software Foundation, either version 3 of the License, or 
+% (at your option) any later version. 
+%
+% FMTS is distributed in the hope that it will be useful, 
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with FMTS.  If not, see <https://www.gnu.org/licenses/>.
+
+% Strength of Materials february 2021 exam. Exam available at https://eva.fing.edu.uy/pluginfile.php/8823/mod_folder/content/0/examen_2021_02_22_letra_y_solucion.pdf?forcedownload=1 for Universidad de la Republica students
+
+clear all, close all, addpath( [ pwd '/../src'] );
+
+l=1 ;
+
+problem_name = 'exam_jul2023_SM2'
+
+% section properties: vector with the cross-section areas
+As = [  .05*.05 ]' ;
+
+% material properties: vector with young moduli values
+Es = [ 20e9 ] ;
+
+% nodal coordinates
+NodsCoord = [ l    l  ;
+              2*l  l  ;
+              0    0  ;
+              l    0  ;
+              2*l  0  ;
+              3*l  0  ] ;
+
+% inode jnode material section
+ElemConec = [ 1 2  1 1 ;
+              1 3  1 1 ; ...
+              1 5  1 1 ; ...
+              2 4  1 1 ; ...
+              2 6  1 1 ; ...
+              1 4  1 1 ; ...
+              2 5  1 1 ; ...
+              3 4  1 1 ; ...
+              4 5  1 1 ; ...
+              5 6  1 1 ] ;
+
+fixeddofs = [ 3*2 2*6-1 2*6  ] ;
+
+NodalLoads = [ 5 0 -5e3 ];
+
+# row vector with the dofs related to supports which are replaced by virtual forces
+virtualforcessupports = [  ] ;
+
+# row vector with the truss elements which are replaced by virtual forces
+virtualforceselements = [ 3 ] ; 
+
+# degree of freedom whose displacement must be determined (leave empty if none)
+unkndispdof = 10 ;
+% -----------------------------
+
+Preprocess
+
+%% 3- Process
+Process
+
+%% 4- Output
+Plots
